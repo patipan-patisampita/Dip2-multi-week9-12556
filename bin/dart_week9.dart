330fefc940071815674bbaea5ece4533533b3539
patipan-patisampita <patipan-patisampita@gmail.com>
2023-07-13 15:24:32 +0700

    update

---
+++ bin/dart_week9.dart
@@ -0,0 +1,14 @@
+import 'package:dart_week9/number_type.dart';
+
+void main(){
+  valueInput();//called method valueInput()
+  numberType();//called method numberType()
+  multiLineText(); //called method multiLineText()
+  convertIntToDouble();
+  isMarry();
+  rawString ();
+}
+
+
+
+
---
+++ lib/number_type.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void rawString (){
+  num prize = 10;
+  String value1 = "The value of price is \t $prize";
+  String value2 = r"The value of price is \t $prize";
+  print(value1);
+  print(value2);
+}
+
+void isMarry(){
+  bool isMarried = true;
+  print("ปล้ายฟ้า Married status: $isMarried");
+}
+void convertIntToDouble(){
+  double num1 = 10.01;
+  int num2 = num1.toInt();
+  print(num2);
+}
+
+//Convert String to double
+void valueInput(){
+  int age = 25;
+  print("ป้อนอายุ:");
+  age = int.parse(stdin.readLineSync()!);
+  print("ปลายฟ้า อายุ: $age");
+}
+
+void numberType(){
+  String name = "ราคาสินค้า";
+  double lemon = 2 * 2.5;
+  int salt = 2 * 10;
+  double total = lemon + salt;
+  print("$name: $total บาท");
+}
+
+void multiLineText(){
+  String vnl = '''
+    This is a book
+    This is a pen
+    This is a cat
+  ''';
+  print(vnl);
+}
